e274f009317e0edc82538f442efe233e7b8102a6
iinnaa99 <iinnaa9911@gmail.com>
2024-11-05 11:55:16 +0900

    Update Solution.java

--- softeer/level1/연탄 배달의 시작/Solution.java
+++ softeer/level1/연탄 배달의 시작/Solution.java
@@ -3,13 +3,34 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("##  ##     ##     #####    #####    ##  ##            ##  ##            ##   ##    ##      ####");
-        System.out.println("##  ##    ####    ##  ##   ##  ##   ##  ##            ##  ##            ### ###   ####    ##  ##");
-        System.out.println("##  ##   ##  ##   ##  ##   ##  ##   ##  ##             ####             #######  ##  ##   ##");
-        System.out.println("######   ######   #####    #####     ####               ##     ######   ## # ##  ######    ####");
-        System.out.println("##  ##   ##  ##   ##       ##         ##               ####             ##   ##  ##  ##       ##");
-        System.out.println("##  ##   ##  ##   ##       ##         ##              ##  ##            ##   ##  ##  ##   ##  ##");
-        System.out.println("##  ##   ##  ##   ##       ##         ##              ##  ##            ##   ##  ##  ##    ####");
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in)); 
+        StringTokenizer st = new StringTokenizer(buf.readLine()); 
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] villages = new int[n];
+
+        st = new StringTokenizer(buf.readLine());
+        for (int i = 0; i < n; i++) {
+            villages[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < n - 1; i++) {
+            int distance = villages[i + 1] - villages[i];
+            if (distance < minDistance) {
+                minDistance = distance;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int distance = villages[i + 1] - villages[i];
+            if (distance == minDistance) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
     }
 }
