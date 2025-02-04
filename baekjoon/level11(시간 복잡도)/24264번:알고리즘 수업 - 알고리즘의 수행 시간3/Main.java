8502a12c213342fdc637b1d9ad07402955922ef5
iinnaa99 <iinnaa9911@gmail.com>
2025-02-04 19:25:18 +0900

    add level11

---
+++ baekjoon/level11(시간 복잡도)/24264번:알고리즘 수업 - 알고리즘의 수행 시간3/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long n = sc.nextLong();
+        sc.close();
+
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
