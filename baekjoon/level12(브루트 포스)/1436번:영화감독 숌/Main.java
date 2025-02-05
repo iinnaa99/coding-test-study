b8c6e5653c83cc59e85ed4474dc7baf63e658222
iinnaa99 <iinnaa9911@gmail.com>
2025-02-05 18:37:23 +0900

    add level12

---
+++ baekjoon/level12(브루트 포스)/1436번:영화감독 숌/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		sc.close();
+
+		int num = 666;
+		int cnt = 1;
+
+		while (cnt != N) {
+			num++;
+			if (String.valueOf(num).contains("666")) {
+				cnt++;
+			}
+		}
+		System.out.println(num);
+	}
+}
