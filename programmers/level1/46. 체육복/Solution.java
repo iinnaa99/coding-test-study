095f235b3fcad9c5129e60d86ef28e7edcff898b
iinnaa99 <iinnaa9911@gmail.com>
2025-03-12 21:47:21 +0900

    250312 add

---
+++ programmers/level1/46. 체육복/Solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int count = n - lost.length;
+        
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    count++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (reserve[j] == lost[i] - 1 || reserve[j] == lost[i] + 1) {
+                    count++;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
